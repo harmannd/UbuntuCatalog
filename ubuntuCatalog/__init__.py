016cfab41abd5519176c4fb202092ce0124e2ca9
root <root@ip-172-26-3-219.us-east-2.compute.internal>
2017-10-05 20:16:33 +0000

    Config settings complete, test displays at domain

--- ubuntuCatalog/__init__.py
+++ ubuntuCatalog/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def hello():
-    return "Hello, I love Digital Ocean!"
+    return "Hello catalog"
 if __name__ == "__main__":
+    app.debug = True
     app.run()
